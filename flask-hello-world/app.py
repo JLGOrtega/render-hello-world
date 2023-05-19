0ed56c1c60e0c372a051c4c3da5244cef5147d54
JLGOrtega <joseluis.gomez@thebridgeschool.es>
2023-05-19 10:22:26 +0200

    add db access

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
+import psycopg2
+from sqlalchemy import create_engine, text
+import pandas as pd
 
 @app.route('/')
 def hello_world():
     return 'Hello, World! CHANGED'
+
+
+@app.route('/db')
+def db():
+    engine = create_engine("postgresql://postgres:fckGDnStyhzL0Y1BsW0J@containers-us-west-122.railway.app:7954/railway")
+    data = pd.read_sql_query(text("SELECT * FROM tabla"), con = engine)
+
+    return data.values.tolist()
